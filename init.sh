6509f30e499c7b73907a72c633b1d957c338df75
duc <1025434218@qq.com>
2018-12-11 15:35:36 +0800

    upgrade 7.2 to 7.3

--- init.sh
+++ init.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-test -f /usr/bin/mysqldump && echo "mysqldump exists" || (apt update && apt install -y mysql-client-5.7)
-# cd /var/www/html && php artisan migrate --force
+cd /var/www/html && php artisan migrate --force && chown -R www-data: storage/ public/
