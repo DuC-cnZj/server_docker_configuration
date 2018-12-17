4c603005d957f1047b0796c05b9a2db008a877e6
duc <1025434218@qq.com>
2018-12-17 17:41:42 +0800

    local

--- configuration/init.sh
+++ configuration/init.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
-cd /var/www/html && php artisan migrate --force && chown -R www-data: storage/ public/
+cd /var/www/html
+bash ./wait-for-it.sh db:3306 -- echo "db is up"
+php artisan migrate --force
+chown -R www-data: storage/ public/
+php artisan db:seed --force
+sleep 30
+php artisan elastic:create-index App\\ES\\ArticleIndexConfigurator > /dev/null 2>&1
+php artisan db:seed --class=ArticleTableSeeder --force
+php artisan scout:import "App\Article" > /dev/null 2>&1
